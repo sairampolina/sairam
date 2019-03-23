f797b2a37d45c0938d11182216232c0ea8274944
sairampolina <polinavenkatasairam@gmail.com>
2019-03-23 17:00:10 +0530

    Added arith

---
+++ arith/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int add(int, int);
+int sub(int, int);
+int main(int argc,char const *argv[])
+{   printf("Add; %d\n",add(10,20));
+   printf("sub; %d\n",sub(10,20));
+   return 0;
+
+}
